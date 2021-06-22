7e25676c9eaa8c3bd760649ec620cdb71dfbda49
dan <dandan682@outlook.com>
2021-06-22 18:34:57 -0500

    Initial Commit

---
+++ NoteApp WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NoteApp WatchKit Extension
+//
+//  Created by yo on 22/06/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NoteApp WatchKit Extension/NoteAppApp.swift
@@ -0,0 +1,19 @@
+//
+//  NoteAppApp.swift
+//  NoteApp WatchKit Extension
+//
+//  Created by yo on 22/06/21.
+//
+
+import SwiftUI
+
+@main
+struct NoteAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
